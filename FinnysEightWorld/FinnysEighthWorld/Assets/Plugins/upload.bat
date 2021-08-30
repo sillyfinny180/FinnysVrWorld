d91f3cfe1aa0384098a51cd5a141fa5bcf692f61
sillyfinny180 <59520462+sillyfinny180@users.noreply.github.com>
2021-08-29 18:38:36 -0700

    Finnys Eighth World
    EarthQuake

---
+++ FinnysEightWorld/FinnysEighthWorld/Assets/Plugins/upload.bat
@@ -0,0 +1,2 @@
+echo off
+curl -v -b cookie -X PUT -F "space_template[zip]=@C:\Users\silly\OneDrive\Desktop\Create worlds-Finny\FinnysEightWorld\FinnysEighthWorld\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1814935072101368415.json
